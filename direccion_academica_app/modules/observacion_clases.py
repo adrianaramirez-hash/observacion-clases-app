0ee34a163dbb37083e32cbb58363b41af3a6b9c7
adrianaramirez-hash <adriana.ramirez@udlondres.com>
2025-11-28 11:09:41 -0600

    Update observacion_clases.py

--- direccion_academica_app/modules/observacion_clases.py
+++ direccion_academica_app/modules/observacion_clases.py
@@ -1,1 +1,7 @@
+import streamlit as st
+
+def pagina_observacion_clases():
+    st.subheader("Observación de clases")
+    st.write("✅ El módulo de Observación de clases se importó correctamente.")
+    st.write("Luego aquí pegaremos el dashboard real.")
 
